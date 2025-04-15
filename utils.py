1b8f738175c0ef7d62bb089acb8e4a7e3e78c752
Manya Wadhwa <manya.wadhwa@utexas.edu>
2025-04-15 16:22:59 -0500

    utils

---
+++ utils.py
@@ -0,0 +1,44 @@
+import re
+
+def remove_numbers_from_sentences(text):
+    if len(text) == 0:
+        return text
+    # Use regex to remove numbers followed by a period and optional space at the start of lines
+    cleaned_text = re.sub(r'^\d+\.\s*', '', text)
+    return cleaned_text
+
+def edit_string_to_not_have_filler_text(text):
+    pattern = r'^\d{1,2}\.'  # ^\d{1,2}\.
+    found = False
+    import copy
+    before = copy.deepcopy(text)
+    attempts = 5
+    current_attempt = 0
+    while not found and current_attempt < attempts:
+        to_check = before.strip().split("\n")[0].strip() if isinstance(before, str) else before[0].strip()
+        # print(to_check)
+        match = re.match(pattern, to_check)
+        if match:
+            found = True
+            current_attempt = attempts
+            # print("keeping...")
+        else:
+            before = "\n".join(before.strip().split("\n")[1:]) if isinstance(before, str) else before[1:].strip()
+            current_attempt += 1
+            # print("not keeping...")
+    found = False
+    current_attempt = 0
+    after = copy.deepcopy(before)
+    while not found and current_attempt < attempts:
+        to_check = after.strip().split("\n")[-1].strip() if isinstance(after, str) else after[-1].strip()
+        # print(to_check)
+        match = re.match(pattern, to_check)
+        if match:
+            found = True
+            current_attempt = attempts
+            # print("keeping...")
+        else:
+            after = "\n".join(after.strip().split("\n")[:-1]) if isinstance(after, str) else after[:-1].strip()
+            current_attempt += 1
+            # print("not keeping...")
+    return after
