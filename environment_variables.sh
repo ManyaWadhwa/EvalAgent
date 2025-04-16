ee00e3613189beae8087cbbe32af6325c374cc2a
Manya Wadhwa <manya.wadhwa@utexas.edu>
2025-04-16 11:38:52 -0500

    environment variables sh

---
+++ environment_variables.sh
@@ -0,0 +1,8 @@
+export OPENAI_API_KEY=
+export GOOGLE_SEARCH_API_KEY=
+export CSE_ID=
+export reddit_client_id=
+export reddit_client_secret=
+export reddit_user_agent=
+export reddit_username=
+export reddit_password=
